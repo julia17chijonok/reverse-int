854d6bb43f1fc27935d64298465e8f02c48e17d2
Julia <chijonok.julia17@gmail.com>
2023-08-14 01:54:50 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n > 0) {
+        let str = String(n);
+        let arr = Array.from(str);
+        let newArr = arr.reverse();
+        let res = +newArr.join('');
+        return res;
+    } else {
+        let str = String(n);
+        let newStr = str.slice(1);
+        let arr = Array.from(newStr);
+        let newArr = arr.reverse();
+        let res = +newArr.join('');
+        return res;
+    }
+};
+
